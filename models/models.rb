5653da10811771985bb300487a53599a7745bb63
Scott Chamberlain <myrmecocystus@gmail.com>
2017-06-20 16:12:42 -0700

    added a route for List (list all species)

--- models/models.rb
+++ models/models.rb
@@ -8,4 +8,29 @@ module Models
   class Iucn < Base; end
   class Usda < Base; end
 
+  SELECT species FROM bien_species_all ORDER BY species;
+
+  class List < Base
+    self.table_name = 'bien_species_all'
+
+    def self.endpoint(params)
+      params.delete_if { |k, v| v.nil? || v.empty? }
+
+      %i(limit offset).each do |p|
+        unless params[p].nil?
+          begin
+            params[p] = Integer(params[p])
+          rescue ArgumentError
+            raise Exception.new("#{p.to_s} is not an integer")
+          end
+        end
+      end
+      raise Exception.new('limit too large (max 1000)') unless (params[:limit] || 0) <= 1000
+      select('species')
+          .order('species')
+          .limit(params[:limit] || 10)
+          .offset(params[:offset])
+    end
+  end
+
 end
